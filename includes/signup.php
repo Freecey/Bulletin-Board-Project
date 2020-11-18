a1ee4a5df26766e78d5f220ceba799810a815cac
Cedric AUDRIT <cedric@audrit.be>
2020-11-18 17:10:25 +0100

    signup.php fix:
    - add check for password and confirm password
    - add hash password
    - add keep feild if password don't match
    - clear all feild if Success
    - add REQUIRED for all input feild
    - fix place of error msg, not match
    - fix place of error creation or Success
    
    todo:
    
    - prez
    - redirect or add link to login
    - ...

--- includes/signup.php
+++ includes/signup.php
@@ -1,25 +1,41 @@
 <?php
+include('header.php');
+
 session_start();
 try {
 	include('connect.php');
 	if(isset($_POST['signup'])){
 		$user_name = $_POST['user_name'];
 		$user_email = $_POST['user_email'];
-		$user_pass = $_POST['user_pass'];
-		$user_fname = $_POST['user_fname'];
+		$user_fname = $_POST['user_fname'];		
 		$user_lname = $_POST['user_lname'];
-        $user_date = date('Y-m-d H:i:s');
+		$user_pass = $_POST['user_pass'];
+		$cpassword = $_POST['cpassword'];
+		if($user_pass == $cpassword) {
+			// echo 'password match OK';
+			$user_pass = hash('sha512', $user_pass);
 
-		$insert = $conn->prepare("INSERT INTO users(user_name,user_email,user_pass,user_fname,user_lname,user_date)
-				values(:user_name, :user_email, :user_pass, :user_fname, :user_lname, :user_date)
-				");
-		$insert->bindParam (':user_name',$user_name);
-		$insert->bindParam (':user_email',$user_email);
-		$insert->bindParam (':user_pass',$user_pass);
-		$insert->bindParam (':user_fname',$user_fname);
-		$insert->bindParam (':user_lname',$user_lname);
-		$insert->bindParam (':user_date',$user_date);
-		$insert->execute();
+			$user_date = date('Y-m-d H:i:s');
+	
+			$insert = $conn->prepare("INSERT INTO users(user_name,user_email,user_pass,user_fname,user_lname,user_date)
+					values(:user_name, :user_email, :user_pass, :user_fname, :user_lname, :user_date)
+					");
+			$insert->bindParam (':user_name',$user_name);
+			$insert->bindParam (':user_email',$user_email);
+			$insert->bindParam (':user_pass',$user_pass);
+			$insert->bindParam (':user_fname',$user_fname);
+			$insert->bindParam (':user_lname',$user_lname);
+			$insert->bindParam (':user_date',$user_date);
+			$insert->execute();
+			$dbResErr = "Sign Up Successfully";
+			unset($user_name);
+			unset($user_email);
+			unset($user_fname);
+			unset($user_lname);
+		} else {
+			$passmatchErr = 'Error ! : password match NOK';
+		}
+
     }	
     
 
@@ -29,26 +45,26 @@ try {
 
 catch (PDOException $e) {
 
-	echo "Error: ". $e -> getMessage();
+	$dbResErr = "Error: ". $e -> getMessage();
 }
 ?>
 
 
-
-
+<!-- Remove BR tag when set prez  -->
 </style>
 <div class ="box">
 Don't have any account yet?
 <h1>Sign Up Here</h1>
+<div><?php echo $dbResErr ?></div>
+<div>* All fields is required</div> 
 <form method="post">
-<input type="text" name = "user_name" placeholder = "User Name"> </br> </br>
-<input type="text" name = "user_email" placeholder = "name@domain.com"> </br> </br>
-<input type="password" name = "user_pass" placeholder = "********"> </br> </br>
-<input type="password" name = "cpassword" placeholder = "********"> </br> </br>
-<input type="text" name = "user_fname" placeholder = "First Name"> </br> </br>
-<input type="text" name = "user_lname" placeholder = "Last Name"> </br> </br>
-
-<input type="submit" name = "signup" Value = "Register">
+<input type="text" name = "user_name" placeholder = "User Name" required value="<?php echo $user_name ?>"> * </br> </br>
+<input type="email" name = "user_email" placeholder = "name@domain.com" required value="<?php echo $user_email ?>"> * </br> </br>
+<input type="password" name = "user_pass" placeholder = "********" required> * <?php echo $passmatchErr;?> </br> </br>
+<input type="password" name = "cpassword" placeholder = "********" required> * </br> </br>
+<input type="text" name = "user_fname" placeholder = "First Name" required value="<?php echo $user_fname ?>"> * </br> </br>
+<input type="text" name = "user_lname" placeholder = "Last Name" required value="<?php echo $user_lname ?>"> * </br> </br>
+<input type="submit" name = "signup" Value = "Register" class="btn">
 
 </form>
 </div>
@@ -56,3 +72,6 @@ Don't have any account yet?
 </div>
 
 
+<?php
+include('footer.php');
+?>
