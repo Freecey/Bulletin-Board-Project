7304ee9561056bc59e2fe5c564596e1df74a276a
Cedric AUDRIT <cedric@audrit.be>
2020-11-18 17:53:42 +0100

    signup.php fix + add Privacy policy
    - add Privacy policy page
    - add link to policy page under signup form
    - add keep feild if password don't match
    - minor placement fix
    
    todo:
    
    - prez
    - redirect or add link to login
    - ...

--- includes/signup.php
+++ includes/signup.php
@@ -51,24 +51,22 @@ catch (PDOException $e) {
 
 
 <!-- Remove BR tag when set prez  -->
-</style>
 <div class ="box">
-Don't have any account yet?
-<h1>Sign Up Here</h1>
-<div><?php echo $dbResErr ?></div>
-<div>* All fields is required</div> 
-<form method="post">
-<input type="text" name = "user_name" placeholder = "User Name" required value="<?php echo $user_name ?>"> * </br> </br>
-<input type="email" name = "user_email" placeholder = "name@domain.com" required value="<?php echo $user_email ?>"> * </br> </br>
-<input type="password" name = "user_pass" placeholder = "********" required> * <?php echo $passmatchErr;?> </br> </br>
-<input type="password" name = "cpassword" placeholder = "********" required> * </br> </br>
-<input type="text" name = "user_fname" placeholder = "First Name" required value="<?php echo $user_fname ?>"> * </br> </br>
-<input type="text" name = "user_lname" placeholder = "Last Name" required value="<?php echo $user_lname ?>"> * </br> </br>
-<input type="submit" name = "signup" Value = "Register" class="btn">
-
-</form>
-</div>
-</div>
+	Don't have any account yet?
+	<h1>Sign Up Here</h1>
+	<div><?php echo $dbResErr ?></div>
+	
+		<form method="post">
+			<input type="text" name = "user_name" placeholder = "User Name" required value="<?php echo $user_name ?>"> * </br> </br>
+			<input type="email" name = "user_email" placeholder = "name@domain.com" required value="<?php echo $user_email ?>"> * </br> </br>
+			<input type="password" name = "user_pass" placeholder = "********" required> * <?php echo $passmatchErr;?> </br> </br>
+			<input type="password" name = "cpassword" placeholder = "********" required> * </br> </br>
+			<input type="text" name = "user_fname" placeholder = "First Name" required value="<?php echo $user_fname ?>"> * </br> </br>
+			<input type="text" name = "user_lname" placeholder = "Last Name" required value="<?php echo $user_lname ?>"> * </br> </br>
+		<input type="submit" name = "signup" Value = "Register" class="btn">
+		</form>
+		<div>* All fields is required</div> 
+		<div><a href="../policy.html">privacy policy</a></div>
 </div>
 
 
