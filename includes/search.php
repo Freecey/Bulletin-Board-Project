4a6a14baaf10c2cd24d0641bcd1067417252a847
KevKsar <cassart.kev@gmail.com>
2020-11-19 11:37:27 +0100

    search-bar in-dev

--- includes/search.php
+++ includes/search.php
@@ -1,12 +1,6 @@
 
-<div class="form-group">
-    <div class="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
-        <div class="input-group">
-            <input type="search" placeholder="Search..." aria-describedby="button-addon1" class="form-control border-0 bg-light">
-            <div class="input-group-append">
-            <button id="button-addon1" type="submit" class="btn btn-link text-primary"><i class="fa fa-search"></i></button>
-            </div>
-        </div>
-    </div>
-</div>
 
+<form action="" methode="POST" >
+    <input type="text" name="search" placeholder="Search...">
+    <button type="submit" name="submite-search">S</button>
+</form>
