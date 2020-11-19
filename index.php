c80f8c4d967756f192120ad4fb9fac2d956f62dc
Tanya Leenders <tanya.leenders@gmail.com>
2020-11-19 10:28:50 +0100

    footer

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>.::Bulletin Board::.</title>
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/main.css" type="text/css">
         <script src="https://kit.fontawesome.com/ad9205c9ea.js" crossorigin="anonymous"></script>
     </head>
     <body>
