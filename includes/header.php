d604aef36ea551bcf051e4475119962d49da10a7
KevKsar <cassart.kev@gmail.com>
2020-11-19 14:54:25 +0100

    header in-dev

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,10 @@
-<header>
-    <h1>Mind your head</h1>
-</header>
+
+<div id="header" class="container-fuide">
+    <div class="row-12">
+        <nav class="nav justify-content-end">
+            <a class="nav-link text-white" href="#">Register</a>
+            <a class="nav-link text-white" href="#">Login</a>
+        </nav>
+        <h1 class="text-center text-white">TRACKER</h1>
+    </div>
+</div>
