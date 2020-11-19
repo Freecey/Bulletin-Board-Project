2675e1da396b13b1ae274bf2326faf064e7c695f
Cedric AUDRIT <cedric@audrit.be>
2020-11-19 09:46:37 +0100

    signup.php fix
    - splite signup page into 2 page for hind for after signup
    
    todo:
    
    - ...

--- includes/signup.php
+++ includes/signup.php
@@ -12,7 +12,7 @@ $usernameErr = '';
 $passmatchErr = '';
 $creationOKClass = '';
 $creationOK = '';
-
+$signupProssComplet = false;
 
 
 session_start();
@@ -49,6 +49,7 @@ try {
 				$insert->execute();
 				$creationOKClass = 'bg-success text-white';
 				$creationOK = 'Sign Up Successfully <a href="../index.php">Click Here to go Home for login</a>';
+				$signupProssComplet = true;
 				unset($user_name);
 				unset($user_email);
 				unset($user_fname);
@@ -75,73 +76,24 @@ catch (PDOException $e) {
 		$dbResErr = '';
 	}
 }
-?>
 
 
-<!-- Remove BR tag when set prez  -->
-<div class ="box">
-	Don't have any account yet?
-	<h1>Sign Up Here</h1>
-	<div><?php echo $dbResErr ?></div>
-	
-		<form method="post">
+if ($signupProssComplet == true) {
+	echo '
+	<div class="form-group '; echo $creationOKClass; echo '">
+	<p>'; echo $creationOK ; echo ' </p>
+
+	</div>
+
+<div>you must be at least 16 years old to register</div>
+<div><a href="../policy.html">Read privacy policy</a></div>
+';
+} else {
+	include('signupform.php');	
+}
 
 
-			<div class="form-group <?php echo $nameclasserr; ?>">
-				<label for="user_name">Username <?php echo $usernameErr;?></label>
-				<input type="text" class="form-control" name = "user_name" placeholder = "User Name" required value="<?php echo $user_name ?>"> 
-			</div>	
-
-			<div class="form-group">
-				<label for="user_email">Email address</label>
-				<input type="email"  class="form-control" name = "user_email" placeholder = "name@domain.com" required value="<?php echo $user_email ?>"  id="exampleInputEmail1" aria-describedby="emailHelp" > 
-				<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-			</div>	
-
-			<div class="form-group">
-				<label for="user_fname">First Name</label>
-				<input type="text" class="form-control" name = "user_fname" placeholder = "First Name" required value="<?php echo $user_fname ?>">
-			</div>	
-			<div class="form-group">
-				<label for="user_lname">Last Name</label>
-				<input type="text" class="form-control" name = "user_lname" placeholder = "Last Name" required value="<?php echo $user_lname ?>">
-			</div>	
-			<div class="form-group <?php echo $pwdclasserr; ?>">
-				<label for="user_pass">Password <?php echo $passmatchErr;?></label>
-				<input type="password" class="form-control" name = "user_pass" placeholder = "********" required>
-				<small id="" class="form-text text-muted">Must be 8-20 characters long.</small>
-			</div>	
-			<div class="form-group  <?php echo $pwdclasserr; ?>">
-				<label for="cpassword">Confirm Password <?php echo $passmatchErr;?></label>
-				<input type="password" class="form-control" name = "cpassword" placeholder = "********" required>
-				<small id="" class="form-text text-muted">retype the same as before.</small>
-			</div>	
-			<input type="submit" class="btn btn-primary" name = "signup" Value = "Register" class="btn btn-primary">
-			<!-- OLD VERSION
-			<div class="form-group">
-				<input type="text" class="form-control" name = "user_name" placeholder = "User Name" required value="<?php echo $user_name ?>"> * </br> 
-				<input type="email"  class="form-control" name = "user_email" placeholder = "name@domain.com" required value="<?php echo $user_email ?>"> * </br> </br>
-				<label for="inputPassword6">Password</label>
-				<input type="password" class="form-control" name = "user_pass" placeholder = "********" required> * <?php echo $passmatchErr;?> </br> </br>
-				<small id="passwordHelpInline" class="text-muted">
-					Must be 8-20 characters long.
-				</small>
-				<input type="password" class="form-control" name = "cpassword" placeholder = "********" required> * </br> </br>
-				<input type="text" class="form-control" name = "user_fname" placeholder = "First Name" required value="<?php echo $user_fname ?>"> * </br> </br>
-				<input type="text" class="form-control" name = "user_lname" placeholder = "Last Name" required value="<?php echo $user_lname ?>"> * </br> </br>
-				<input type="submit" class="btn btn-primary" name = "signup" Value = "Register" class="btn btn-primary">
-			</div> -->
-		</form>
-			<div class="form-group <?php echo $creationOKClass; ?>">
-				<p><?php echo $creationOK; ?></p>
-			</div>	
 
 
-		<div>* All fields is required</div> 
-		<div><a href="../policy.html">Read privacy policy</a></div>
-</div>
-
-
-<?php
 include('footer.php');
 ?>
