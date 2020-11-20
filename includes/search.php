f7c5bc83e12d059faca66bfbbdec47d0751eb8f4
KevKsar <cassart.kev@gmail.com>
2020-11-20 11:06:55 +0100

    search_bar in-dev

--- includes/search.php
+++ includes/search.php
@@ -1,6 +1,29 @@
 
+<?php
+include 'connect.php';
+
+$response = $conn->query('SELECT * FROM topics');
+
+if (isset($_GET['search']) AND !empty($_GET['search'])) {
+    $search = htmlspecialchars($_GET['search']);
+    $response = $conn->query('SELECT * FROM topics, users, boards, posts WHERE * LIKE "%' .$search. '%"');
+}
+?>
 
 <form action="" methode="POST" >
     <input type="text" name="search" placeholder="Search...">
-    <button type="submit" name="submite-search">S</button>
+    <button type="submit" name="submit"><i class="fas fa-search"></i></button>
 </form>
+
+<?php
+while ($datas = $response -> fetch()) 
+{
+?>
+    <p>
+        <?php echo $datas['']; ?>
+        
+    <?php
+    }
+    ?>
+
+
