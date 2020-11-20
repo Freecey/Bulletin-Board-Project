d688623468ad95135c506a79649c9b43dfeee3d5
KevKsar <cassart.kev@gmail.com>
2020-11-20 13:19:07 +0100

    serach-bar in-dev

--- includes/search.php
+++ includes/search.php
@@ -2,11 +2,11 @@
 <?php
     include 'connect.php';
 
-    $response = $conn->query('SELECT *  FROM topics, users, boards, posts');
+    $response = $conn->query('SELECT topic_subject  FROM topics, users, boards, posts');
 
     if (isset($_GET['search']) AND !empty($_GET['search'])) {
         $search = htmlspecialchars($_GET['search']);
-        $response = $conn->query('SELECT * FROM topics, users, boards, posts WHERE topic_subject LIKE "%'.$search.'%"');
+        $response = $conn->query('SELECT topic_subject FROM topics, users, boards, posts WHERE topic_subject LIKE "%'.$search.'%"');
     }
 ?>
 
@@ -19,12 +19,7 @@
 
 <ul>
     <?php while ($datas = $response->fetch()){ ?>
-        <li><?= $datas['topic_id'] ?></li>
         <li><?= $datas['topic_subject'] ?></li>
-        <li><?= $datas['topic_image'] ?></li>
-        <li><?= $datas['topic_date'] ?></li>
-        <li><?= $datas['topic_date_upd'] ?></li>
-        <li><?= $datas['topic_board'] ?></li>
-        <li><?= $datas['topic_by'] ?></li>
+
     <?php } ?>
 </ul>
