cb65cacabd65c5a1acda5622f8ae11c3eae64cc8
Alan Massaro <alan.massar@gmail.com>
2020-11-20 12:35:55 +0100

    reorganize header structure to prepare it for responsive menu hamburger

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,11 @@
-
-<div id="header" class="container-fuide">
-    <div class="row-12">
-        <nav class="nav justify-content-end">
-            <a class="nav-link text-white" href="includes/signup.php"><i class="far fa-file-alt"></i> Register</a>
-            <a class="nav-link text-white" href="#login"><i class="fas fa-sign-in-alt"></i> Login</a>
-        </nav>
-        <h1 class="text-center text-white display-1">TRACKER</h1>
+<section id="header" class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <nav class="nav justify-content-end">
+                <a class="nav-link text-white" href="includes/signup.php"><i class="far fa-file-alt"></i> Register</a>
+                <a class="nav-link text-white" href="#"><i class="fas fa-sign-in-alt"></i> Login</a>
+            </nav>
+            <h1 class="text-center text-white display-1">TRACKER</h1>
+        </div>
     </div>
-</div>
+</section>
