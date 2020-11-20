34dc03b31c1c582fda093eeb0abde5a45a60b4ed
Tanya Leenders <tanya.leenders@gmail.com>
2020-11-20 11:02:49 +0100

    update breadcrumb

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,34 +1,30 @@
-<?php
-// WORK IN PRGRESS, NEEDS SOME UPDATES
-$baseUrl = "index.php"
-function createBreadCrumbs() {
-    global $baseUrl;
-    $urlParse = parse_url( $baseUrl.$_SERVER["REQUEST_URI"], PHP_URL_PATH ); //appends the http://.../ to /folder1/subfolder/file.php
+<ol class="breadcrumb">
+    <?php 
 
-    $crumbs = explode( "/", $urlParse ); //transforms /folder/folder/file.php -> array['','folder','subfolder','file.php']
-    
-    $REMOVE = array( "", "index.php" ); //an array that will hold strings to be removed -> array['folder','subfolder','file.php']
-    
-    $crumbs = array_diff( $crumbs, $REMOVE );// remove the elements who's values are stored in REMOVE array
+    $breadcrumb;
+    $separator = ' / ';
+    $domain = 'jenesaispas'
+    function build($array) {
+        $crumbs = array_merge(array('home' => ''), $array);
+        $count = 0;
 
-    $totalCrumbs = count( $crumbs ); //counts the number of items in the array $crumbs
-    $count = 0;
+        foreach($crumbs as $title => $link) {
+            $this->breadcrumb .= '
+            <li>
+                <a href"'.$this->domain. '/' .link.'" itemprop="url">
+                    <span itemprop="title">'.$title.'</span>
+                </a> 
+            </li>'
 
-    //this is the subdirectory we are on the folder...
-    $uri = "/cds/garibaldi/";
-    foreach ( $crumbs as $crumb ) {
         $count++;
 
-        if ( $count!=$totalCrumbs ) { //checks if we are at the end of the urlParse
-            //not at end so, lets append an > to the url, and then for the href lets add the additonal crumb to the uri string...
-            echo '<a href="'.$uri.$crumb.'">'.ucwords( str_replace( array( "-", ".php", "_" ), array( " ", "", " " ), $crumb ) . '</a> > ' );
-        } else {
-            //at the end of the uri string, we don't need >, and for href lets use the requesting URI (works well when pass arguments and queries ?,# in URI)
-            echo '<a href="'.$_SERVER["REQUEST_URI"].'">'.ucwords( str_replace( array( "-", ".php", "_" ), array( " ", "", " " ), $crumb ) . '</a>' );
+        if ($count !== count($crumbs)) {
+            $this->breadcrumb .= $this->separator;
         }
-        //add the string to the uri and store it.
-        $uri = $uri.$crumb.'/';
+        }
     }
-}
-?>
-<p>test test test</p>
+    return $this->breadcrumb;
+
+    ?>
+  <li class="guidemap"><a href= <?php echo $homePath . 'guides/map.php"' ?> >map</a></li>
+</ol>
