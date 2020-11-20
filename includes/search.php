7f60a39d73bd0c71a12f46f4624dc2a3bba09d65
KevKsar <cassart.kev@gmail.com>
2020-11-20 14:10:39 +0100

    search-bar in-dev

--- includes/search.php
+++ includes/search.php
@@ -11,10 +11,16 @@
 ?>
 
 
-<form action="" methode="GET" >
-    <input type="text" name="search" placeholder="Search..."  class="form-control form-rounded">
+<form atcion="" methode="GET "class="form-group">	
+    <div class="p-1 bg-light rounded rounded-pill shadow-sm mb-4">	
+        <div class="input-group">	
+            <input type="search" name="search" placeholder="Search..." aria-describedby="button-addon1" class="form-control border-0 bg-light">	
+            <div class="input-group-append">	
+            <button id="button-addon1" type="submit" class="btn btn-link text-primary"><i class="fa fa-search"></i></button>	
+            </div>	
+        </div>	
+    </div>	
 </form>
-
 
 <ul>
     <?php while ($datas = $response->fetch()){ ?>
