0735651de7537f5069673913c2af15545dc981bb
Tanya Leenders <tanya.leenders@gmail.com>
2020-11-22 18:08:48 +0100

    corrections: hovers, privacy link

--- includes/signin.php
+++ includes/signin.php
@@ -1,3 +1,80 @@
-<aside style="border: 1px solid coral;">
-<p>Need to login?</p>
+<?php
+//signin.php
+session_start();
+// echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
+try {
+	include('connect.php');
+	if(isset($_POST['signin_main'])){
+		$user_email = $_POST['user_email'];
+        $user_pass = $_POST['user_pass'];
+        $user_pass = hash('sha512', $user_pass);
+        $Login_date = date('Y-m-d H:i:s');
+        // echo $Login_date;
+
+		$SignInSELECT = $conn->prepare("SELECT*FROM users where user_email='$user_email' and user_pass='$user_pass' LIMIT 1");
+		$SignInSELECT->setFetchMode(PDO::FETCH_ASSOC);
+		$SignInSELECT->execute();
+		$SignInDATA=$SignInSELECT->fetch();
+		if($SignInDATA['user_email']!=$user_email and $SignInDATA['user_pass']!=$user_pass)
+		{
+            // $loginOK = false;
+            echo "Invalid username or Password";
+		}
+		elseif($SignInDATA['user_email']==$user_email and $SignInDATA['user_pass']==$user_pass)
+		{
+
+            $loginOK = true;
+            $loginOK = true;
+			$_SESSION['user_email'] = $user_email;
+            $_SESSION['loginOK'] = $loginOK;
+            $_SESSION['user_level'] = $SignInDATA['user_level'];
+            $_SESSION['user_name'] = $SignInDATA['user_name'];
+            $_SESSION['user_id'] = $SignInDATA['user_id'];
+            $_SESSION['user_gravatar'] = $SignInDATA['user_gravatar'];
+            $user_ID = $SignInDATA['user_id'];
+            
+            $email = $_SESSION['user_email'];
+            $size = '';
+            include('includes/gravatars.php');
+            $user_gravatar = $grav_url;
+
+            $UPDATEQuerySQL2 = "UPDATE `users` SET `user_datelastlog` = '$Login_date',`user_gravatar` = '$user_gravatar'  WHERE `users`.`user_id` = $user_ID";
+            $SignInINSERT= $conn->prepare($UPDATEQuerySQL2);
+            $SignInINSERT->execute();
+
+			// header("location:profile.php");
+		}
+	}
+}
+catch (PDOException $Exception) {
+
+    echo "Error: ". $Exception -> getMessage();
+
+}
+
+
+if ($_SESSION['loginOK']  == true) {
+    echo '<div class="m-2">';
+    echo 'Welcome ';echo $_SESSION['user_name'];
+    echo '</div>';
+    echo '<a class="text-white" href="profile.php">
+    <div class="my-2 btn btn-primary btn-block rounded-pill" >
+        Your Profil
+    </div></a>';
+
+
+
+    echo '
+    <a class="text-white" href="logout.php">
+    <div class="my-2 btn btn-primary btn-block rounded-pill" >
+        Logout
+    </div></a>';
+    // echo '<pre>' . print_r($SignInDATA, TRUE) . '</pre>';
+    // echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
+} else {
+    include('includes/signinform.php');
+}
+?>
+
+
 </aside>
---
+++ topics.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>.::Bulletin Board::.</title>
+        <link rel="stylesheet" href="css/main.css" type="text/css">
+        <script src="https://kit.fontawesome.com/ad9205c9ea.js" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+    </head>
+    <body>
+        <?php include('includes/connect.php') ?>
+        <?php include('includes/header.php'); ?>
+        <main class="pr-sm-5 pl-sm-5">
+            <div class="container-fluid shadow rounded-lg" id="content">
+                <div class="row">
+                    <div class="col-12">
+                        <?php include('includes/breadcrumb.php'); ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-10 col-md-9">
+                    <?php
+                        $req_topics = $conn->query('SELECT * FROM topics ORDER BY topic_id');
+                        if (!$req_topics) {
+                            echo 'Unable to display the topics' .mysql_error();
+                        } else {
+                        ?>
+
+                        <section class="mb-3" id="topics">
+                            
+                            <article class="container-fluid">
+                              
+                                <div class="row">
+                                    <div class="col">
+                                        <h2>Topics' List</h2>
+                                    </div>
+                                </div>
+
+                                <div class="alert alert-danger m-2" role="alert">
+                                    Forum rules
+                                </div>
+
+                                <div class="btn-and-search row">
+                                    <button class="col-2 btn btn-primary btn-block rounded-pill"> New Topic <i class="fas fa-pencil-alt"></i> </button>
+                                    <div class="col-2"> <?php include('includes/search.php'); ?> </div>
+                                </div>
+
+                                <div class="row card bg-light rounded-lg m-2 pb-3">
+                                    <div class="card-header d-flex align-items-center">
+                                        <div class="card-header__element col-7">
+                                            <p class="h6 mb-1">Announcements</p>
+                                        </div>
+                                        <div class="card-header__element col-2"> <i class="fas fa-comments"></i> </div>
+                                        <div class="card-header__element col-1"> <i class="far fa-eye"></i> </div>
+                                        <div class="card-header__element col-2"> <i class="far fa-clock"></i> </div>
+                                    </div>
+
+                                    <div id="announce-list" class="card-body">
+                                    <?php
+                                        $req_announce = $conn->query('SELECT * FROM announce ORDER BY ann_id');
+                                        if (!$req_announce) {
+                                            echo 'Unable to display the announces' .mysql_error();
+                                        } else {
+                                            while ($ann = $req_announce->fetch())
+                                            { 
+                                            ?>
+                                            <div class="card m-1">
+                                                <div class="card border-0">
+                                                    <div class="ann-list-item card-body w-100 d-flex">
+                                                        <div class="col-7">
+                                                            <?php echo '<a href="../Bulletin-Board-Project/announce-content.php?id=' . $ann['ann_id'] . '">' . $ann['ann_subject'] . '</a>'?>
+                                                        </div>
+                                                        <div class="ann-details col-2">
+                                                            Comments
+                                                            <!-- TODO: use a request / 'comments linked to this announce' count -->
+                                                        </div>
+                                                        <div class="ann-details col-1">
+                                                            Views
+                                                            <!-- TODO: create a view count? -->
+                                                        </div>
+                                                        <div class="ann-details col-2">
+                                                            Date
+                                                            <!-- TODO: use a request / ann_by and ann_date -->
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <?php
+                                            }
+                                        }
+                                        $req_announce->closeCursor();
+                                        ?>
+                                    </div>
+                                </div>
+
+                                <div class="row card bg-light rounded-lg m-2 pb-3">
+                                    <div class="card-header d-flex">
+                                        <div class="col-7">
+                                            <p class="h6 mb-1">Topics' List</p>
+                                        </div>
+                                        <div class="col-2"> <i class="fas fa-comments"></i> </div>
+                                        <div class="col-1"> <i class="far fa-eye"></i> </div>
+                                        <div class="col-2"> <i class="far fa-clock"></i> </div>
+                                    </div>
+
+                                    <div id="topics-list" class="card-body">
+                                        <?php
+                                        while ($topic = $req_topics->fetch())
+                                        { 
+                                        ?>
+
+                                        <div class="card m-1">
+                                            <div class="card border-0">
+                                                <div class="topic-list-item card-body w-100 d-flex">
+                                                    <div class="col-7">
+                                                        <?php echo '<a href="../Bulletin-Board-Project/comments.php?id=' . $topic['topic_id'] . '">' . $topic['topic_subject'] . '</a>'?>
+                                                    </div>
+                                                    <div class="topic-details col-2">
+                                                        Comments
+                                                        <!-- TODO: use a request / 'post linked to this topic' count -->
+                                                    </div>
+                                                    <div class="topic-details col-1">
+                                                        Views
+                                                        <!-- TODO: create a view count? -->
+                                                    </div>
+                                                    <div class="topic-details col-2">
+                                                        Date
+                                                        <!-- TODO: use a request / topic_by and topic_date -->
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php
+                                        }
+                                        $req_topics->closeCursor();
+                                        ?>
+                                    </div>
+                                </div>
+                            </article>
+                            
+                        </section>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="col-xl-2 col-md-3 d-none d-md-block">
+                        <?php include('includes/search.php'); ?>
+                        <?php include('includes/signin.php'); ?>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php include('includes/footer.php'); ?>
+        <div id="scroll-up-btn" class="d-flex justify-content-center align-items-center" data-toggle="tooltip" data-placement="top" title="Go back to the top">
+            <a href="#top"><i class="fas fa-arrow-up scroll-up-btn__icon"></i></a>
+        </div>
+
+        <script type="text/javascript" src="scroll-up-btn.js"></script>
+    </body>
+</html>
