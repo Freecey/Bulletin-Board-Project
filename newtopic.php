0b77eb4d4a69a2c38a2f1a67b3ee7ad40415bdd3
Cedric AUDRIT <cedric@audrit.be>
2020-11-25 22:51:41 +0100

    create new topic page
    - change link on topics.php to newtopic (add a tag and change button do div with p-3 for keep same prez)
    - use $_GET[boardID] for keep origin board ID
    - change security setting in session.php and sessionadmin go to precedent page or if none go /
    - create topicnew.php for create a topic and msg on same time.

--- newtopic.php
+++ newtopic.php
@@ -1,17 +1,11 @@
 <?php 
 //newtopic.php
-
+include 'includes/session.php';
 include 'includes/1head.php';
 include 'includes/2body.php';
 
-if (empty($_GET['view_user_id'])) {
-    include 'includes/topicnew_view.php';
-    include 'includes/topicnew_form.php';
-
-} else {
-    include 'includes/member_view.php';
-    include 'includes/member_form.php';
-}
+include 'includes/topicnew_view.php';
+include 'includes/topicnew_form.php';
 
 include 'includes/3body.php';
 include 'includes/4foot.php';
