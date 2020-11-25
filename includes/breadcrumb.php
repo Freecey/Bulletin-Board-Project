1d6a1a7d1adf9788e8161fc41802e863d470ade9
Alan Massaro <alan.massar@gmail.com>
2020-11-25 16:06:48 +0100

    track remote branch

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,39 +1,35 @@
-<p>Home / Board / Topic</p>
+<?php
 
+// This function will take $_SERVER['REQUEST_URI'] and build a breadcrumb based on the user's current path
+function breadcrumbs($separator = ' &lsaquo; ', $home = '<i class="fas fa-home"></i> Home') {
+    $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+    $key = array_search('comments.php', $path);
+    if ($key) {
+        echo $key;
+        echo 'comments trouvé';
+        array_splice($path, $key-1, 0, 'topics.php');
+    }
+    echo
+    $base = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+    $breadcrumbs = Array("<a href=\"$base\">$home</a>");
+    $pathkeys = array_keys($path);
+    $last = end($pathkeys);
 
-    <?php 
-        // class breadcrumb {
+    foreach ($path AS $x => $crumb) {
+        $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
 
-        //     private $breadcrumb;
+        if ($x != $last)
+            $breadcrumbs[] = "<a href=\"$base$crumb\">$title</a>";
+        else
+            $breadcrumbs[] = $title;
+    }
+    return implode($separator, $breadcrumbs);
+}
 
-        //     private $separator = ' / ';
+?>
 
-        //     private $domain = 'http://localhost/Bulletin-Board-Project/';
-
-        //     public function build($array) {
-                
-        //         $crumbs = array_merge(array('Home' => ''), $array);
-
-        //         $count = 0;
-
-        //         foreach($crumbs as $title => $link) {
-        //             $this->breadcrumb .= '
-        //             <li>
-        //                 <a href="'.$this->domain. '/'.$link.'" itemprop="url">
-        //                 <span itemprop="title">'.$title.'</span>
-        //                 </a>
-        //             </li>';
-
-        //             $count++;
-
-        //             if($count !== count($crumbs)) {
-        //                 $this->breadcrumb .= $this->separator;
-        //             }
-        //         }
-        //         return $this->breadcrumb;
-        //     }
-        // }
-
-        // $breadcrumb = new breadcrumb();
-        // echo $breadcrumb->build(array());
-    ?>
+<nav>
+    <ol class="breadcrumb bg-white mt-2">
+        <li class="breadcrumb-item"><a href="#"><?= breadcrumbs() ?></a></li>
+    </ol>
+</nav>
