e4dfa32462f8d2e5ddaef2750e7460555a4222f4
Alan Massaro <alan.massar@gmail.com>
2020-11-26 14:07:22 +0100

    fix load JS with disabling breadcrumb footer

--- comments.php
+++ comments.php
@@ -112,15 +112,7 @@
                             </div>
                             <?php require('includes/posts_pagination_reply.php'); ?>
                         </section>
-                        <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-                        <script type="text/javascript">
-                            let posts = document.getElementsByClassName('post-content');
-                            
-                            Array.from(posts).forEach(post => {
-                                const comment = post.innerHTML;
-                                post.innerHTML = marked(comment);
-                            });
-                        </script>
+                        
                     </div>
                     <div class="col-xl-3 col-md-4 d-none d-md-block">
                         <?php include('includes/search.php'); ?>
@@ -131,15 +123,25 @@
                 </div>
             </div>
         </main>
+        
         <?php include('includes/footer.php'); ?>
+        
         <div id="scroll-up-btn" class="d-flex justify-content-center align-items-center" data-toggle="tooltip" data-placement="top" title="Go back to the top">
             <a href="#top"><i class="fas fa-arrow-up scroll-up-btn__icon"></i></a>
         </div>
-        
-        
         <script>const simplemde = new SimpleMDE();</script>
         <script type="text/javascript" src="scroll-up-btn.js"></script>
-        
+        <script type="text/javascript" src="./node_modules/marked/marked.min.js"></script>
+        <script type="text/javascript" src="./node_modules/dompurify/dist/purify.min.js"></script>
+        <script type="text/javascript">
+            let posts = document.getElementsByClassName('post-content');
+            
+            Array.from(posts).forEach(post => {
+                const comment = post.innerHTML;
+                const cleanComment = DOMPurify.sanitize(comment)
+                post.innerHTML = marked(cleanComment);
+            });
+        </script>
         
     </body>
 </html>
