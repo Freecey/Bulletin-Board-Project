9ba3daa6ab8bd89803b07b21ef1605febd62a701
Cedric AUDRIT <cedric@audrit.be>
2020-11-26 15:16:00 +0100

    create site setting page with basic sitename + mail

--- includes/admin/sidemenu.php
+++ includes/admin/sidemenu.php
@@ -12,7 +12,7 @@
         <a class="text-white" href="users.php">
         <div class="my-2  btn btn-primary btn-block rounded-pill" >Edit Users</div></a>
         <a class="text-white" href="setting.php">
-        <div class="my-2  btn btn-primary btn-block rounded-pill" >Site Setting (soon)</div></a>
+        <div class="my-2  btn btn-primary btn-block rounded-pill" >Site Setting</div></a>
         <a class="text-white" href="../">
         <div class="my-2  btn btn-secondary btn-block rounded-pill" >Site Home</div></a>
         <a class="text-white" href="javascript:history.go(-1)">
