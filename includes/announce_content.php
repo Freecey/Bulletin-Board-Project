db8d9578d6c2a9344e659061c867a81130826307
Cedric AUDRIT <cedric@audrit.be>
2020-11-27 16:47:55 +0100

    img fix size announce

--- includes/announce_content.php
+++ includes/announce_content.php
@@ -67,7 +67,7 @@ $UserByDATA=$query->fetch();
                                             <div class="col-12 col-sm-5 col-md-3 col-lg-2">
                                                 <div class="row mb-2 text-md-center">
                                                     <div class="col-4 col-md-3 col-lg-12">
-                                                        <img class="avatar rounded-circle" src="<?= $UserByDATA['user_gravatar'] ?>" alt="<?= htmlspecialchars($UserByDATA['user_name']) ?>'s gravatar">
+                                                        <img class="avatar rounded-circle" src="<?= $UserByDATA['user_gravatar'] ?>" alt="<?= htmlspecialchars($UserByDATA['user_name']) ?>'s gravatar" width="90">
                                                     </div>
                                                     <div class="col-8 col-md-9 col-lg-12">
                                                         <p class="mt-3 mb-0"><a href=member.php?view_user_id=<?php echo $UserByDATA['user_id'] ;?>><strong><?= htmlspecialchars($UserByDATA['user_name']) ?></strong></a></p>
