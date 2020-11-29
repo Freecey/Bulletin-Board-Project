78c550e3f93309f06bb47e3c7761ba1b65a3b570
Cedric AUDRIT <cedric@audrit.be>
2020-11-29 01:01:32 +0100

    login fail log
    - create table for login fail
    - create function for log fail login (store IP, Browser/OS, URLfrom, Datetime, email and password try)

--- includes/signin.php
+++ includes/signin.php
@@ -19,6 +19,7 @@ try {
 		if($SignInDATA['user_email']!=$user_email and $SignInDATA['user_pass']!=$user_pass)
 		{
             // $loginOK = false;
+            include 'includes/function/loginfail.php';
             echo '<div class="bg-danger text-white text-center"> Invalid username or Password</div>';
 		}elseif($SignInDATA['user_active'] == 2)
 		{
