c9e5958e7824bbe5662f278c283a35276c4a12a3
Cedric AUDRIT <cedric@audrit.be>
2020-11-29 02:09:26 +0100

    test mail

--- includes/contact_view.php
+++ includes/contact_view.php
@@ -1,68 +1,50 @@
 <?php
-//contact_view.php
-
-//include 'includes/getdata/sitesettingMail.php';
-// use $SETTINGdata[]
-
-//Load PHPMailer
-// include 'includes/PHPMailer/Exception.php';
-// include 'includes/PHPMailer/PHPMailer.php';
-// include 'includes/PHPMailer/SMTP.php';
-// include  'includes/PHPMailer/OAuth.php';
-
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
 require 'vendor/autoload.php';
 
-echo '123';
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-// noreply BBS
-// noreply_bbs-queen@neant.be
-// wKeUr4bXj6
-// PORT 25  587 465
-
-$mail = new PHPMailer;
-//$mail = new PHPMailer();
-echo '555555555';
 try {
     //Server settings
-$mail->IsSMTP();
-$mail->CharSet = 'UTF-8';
-echo '4444444444';
-$mail->Host       = "send.one.com"; // SMTP server example
-$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->Port       = 465;                    // set the SMTP port for the GMAIL server
-//$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
-$mail->Username   = "bbs-queen@iweep.be"; // SMTP account username example
-$mail->Password   = "123456Az.";        // SMTP account password example
-$mail->SMTPSecure = 'tls';   
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = '';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = False;                                   // Enable SMTP authentication
+    $mail->UserName   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
+    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPAutoTLS = True;
+    //$mail->SMTPSecure = false;
+    $mail->AuthType   = 'LOGIN' ;                   // CRAM-MD5, LOGIN, PLAIN, XOAUTH2. 
 
-//Recipients
-$mail->setFrom('bbs-queen@iweep.be', 'BBS-QUEEN - Cedric');
-$mail->addAddress('cedric@audrit.be', 'Cedric AUDRIT');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
+    //Recipients
+    $mail->setFrom('cedric@neant.be', 'BBS-Queen - Cedric AUDRIT');
+    $mail->addAddress('cedric@audrit.be', 'Cedric');     // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
-//// Attachments
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject 123';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b><br>phpMailer 123';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients, phpMailer';
 
-// Content
-$mail->isHTML(true);                                // Set email format to HTML
-$mail->Subject = 'this is a test';
-$mail->Body    = 'test mail form phpmail<b>in bold!</b>';
-$mail->AltBody = 'test mail form phpmail';
-echo '4444444444';  
-echo '123123';
-$mail->send();
-echo 'Message has been sent';
+    $mail->send();
+    echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-
-
-?>
