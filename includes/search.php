bd64130431d97c6784bf138b2b3a94ab3ca203b3
Cedric AUDRIT <cedric@audrit.be>
2020-12-02 11:02:12 +0100

    fix search

--- includes/search.php
+++ includes/search.php
@@ -1,5 +1,5 @@
 
-<form atcion="" methode="GET "class="form-group">	
+<form action="results_page.php?search=<?php echo $_GET['Email'];?>" methode="GET" class="form-group">	
     <div class="p-1 bg-light rounded rounded-pill shadow-sm mb-4">	
         <div class="input-group">	
             <input type="search" name="search" placeholder="Search..." aria-describedby="button-addon1" class="form-control border-0 bg-light">	
