0716ab8d38a8181021faf569572c4c9923537452
Alan Massaro <alan.massar@gmail.com>
2020-12-03 12:15:24 +0100

    clean Code Session 2

--- includes/postedit_view.php
+++ includes/postedit_view.php
@@ -10,7 +10,7 @@ $select = $conn->prepare("SELECT*FROM posts where post_id=$PostEdit_ID AND post_
 $select->setFetchMode(PDO::FETCH_ASSOC);
 $select->execute();
 $PostDATA=$select->fetch();
-$topic_id=$PostDATA[post_topic];
+$topic_id=$PostDATA['post_topic'];
 
 $select2 = $conn->prepare("SELECT*FROM topics where topic_id=$PostDATA[post_topic] LIMIT 1");
 $select2->setFetchMode(PDO::FETCH_ASSOC);
@@ -18,7 +18,7 @@ $select2->execute();
 $Topic_info=$select2->fetch();
 
 
-if($_SESSION[PostUPDATEComplet] == true ){
+if($_SESSION['PostUPDATEComplet'] == true ){
     $UpdateOKClass = 'bg-success text-white';
     $UpdateOK = 'Board Update Successfully';
     unset($_SESSION['BoardUPDATEComplet']);
@@ -45,13 +45,13 @@ try {
         $nameclasserr = '';
         $UPD_post_date_update = date('Y-m-d H:i:s'); 	 // to ADD QUERY
         $UPD_post_id = $PostEdit_ID;    // DON'T NOT UPDATE 
-        $UPD_post_by = $_SESSION[user_id]; // DON'T NOT UPDATE 
+        $UPD_post_by = $_SESSION['user_id']; // DON'T NOT UPDATE 
         $SetDELQuerySQL = "UPDATE `posts` SET `post_deleted` = '1' WHERE `posts`.`post_id` = $UPD_post_id";
         $Post_SetDelINSERT= $conn->prepare($SetDELQuerySQL);
         echo '123456';
         $Post_SetDelINSERT->execute();
         $_SESSION['SetDELComplet'] = true;
-        header("location:comments.php?id=$topic_id#$UPD_post_id");  
+        header("location:comments.php?id=".$topic_id."#".$UPD_post_id);  
         // header("Refresh:0");
     }
 
