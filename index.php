be110bcd4a5fd00c62ce5b5b01e5dc89f3a6da79
Alan Massaro <alan.massar@gmail.com>
2020-12-07 17:01:08 +0100

    update design

--- index.php
+++ index.php
@@ -103,13 +103,13 @@
                         </section>
 
                     </div>
-                    <div class="col-xl-3 col-md-4 d-none d-md-block">
+                    <aside class="col-xl-3 col-md-4 d-none d-md-block">
                         <?php include('includes/search.php'); ?>
                         <?php include('includes/signin.php'); ?>
                         <?php include('includes/sidebutton2.php'); ?>
                         <?php include('includes/last-posts.php'); ?>
                         <?php include('includes/last-active-user.php'); ?>
-                    </div>
+                    </aside>
                 </div>
             </div>
         </main>
