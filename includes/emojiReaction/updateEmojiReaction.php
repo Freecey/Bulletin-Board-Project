b5f9856774c5d0254c2f642305ba94ebd2fce0f5
Alan Massaro <alan.massar@gmail.com>
2020-12-11 11:44:12 +0100

    Added the delete button

--- includes/emojiReaction/updateEmojiReaction.php
+++ includes/emojiReaction/updateEmojiReaction.php
@@ -8,36 +8,25 @@
     }
 
     $groupReactions = array();
-    
-
-
     $reactions = getReactions($postId);
     foreach($reactions as $reaction) {
         $groupReactions[$reaction['postreact_content']][] = $reaction;
     }
     
-
     foreach($groupReactions as $reaction) {
         // print("<pre>".print_r($reaction,true)."</pre>");
         $users = '';
         foreach($reaction as $name) {
             $user = getUser($name['postreact_user']);
-            $users .= '<p>'. $user['user_name'] .'</p>';
+            $users .= $user['user_name']. ' ';
         }
     ?>
-    <button type="button" class="btn btn-light btn-sm reactionButton" data-toggle="tooltip" data-placement="top" title="<?= $users ?>" onclick="deleteEmojiButton(<?= $reaction['postreact_id']; ?>, <?= $reaction['postreact_post']; ?>)">
+    <button type="button" class="btn btn-light btn-sm reactionButton" data-toggle="tooltip" data-placement="top" title="<?= $users ?>" onclick="deleteEmojiButton(<?= $reaction[0]['postreact_id']; ?>, <?= $reaction[0]['postreact_post']; ?>)">
     <?= $reaction[0]['postreact_content']; ?><span class="badge"><?= count($reaction) ?></span>
     </button>
     <?php
     }
-    while($reaction = $reactions->fetch()) {
-        $user = getUser($reaction['postreact_user']);
-?>
-<button type="button" class="btn btn-light btn-sm reactionButton" data-toggle="tooltip" data-placement="top" data-original-title="<?= $user['user_name']; ?>" onclick="deleteEmojiButton(<?= $reaction['postreact_id']; ?>, <?= $reaction['postreact_post']; ?>)">
-    <?= $reaction['postreact_content']; ?><span class="badge">1</span>
-</button>
-<?php  
-    }
+    
 ?>
 <script>
     $('[data-toggle="tooltip"]').tooltip();
