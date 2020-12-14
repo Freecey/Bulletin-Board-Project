b76b07b097cd8f361fb382b5867ec95b9f70776f
Tanya Leenders <tanya.leenders@gmail.com>
2020-12-14 12:23:35 +0100

    fixed dark theme

--- includes/last-active-user.php
+++ includes/last-active-user.php
@@ -16,17 +16,17 @@
                 while ($user = $req_users->fetch())
                 {
             ?>
-            <a class="card border-0 text-center mb-lg-2 mb-1" href="/member.php?view_user_id=<?= $user['user_id'];?>">
+            <a class="user-card card border-0 text-center mb-lg-2 mb-1" href="/member.php?view_user_id=<?= $user['user_id'];?>">
                 <div class="card-body">
-                    <div class="d-flex flex-column flex-lg-row">
+                    <div class="user-list-item d-flex flex-column flex-lg-row">
                         
                         <div>
-                            <img class="avatar-sm rounded-circle" alt="" src="data:image/webp;base64,<?php echo base64_encode($user['user_imgdata']); ?>" width="48" alt="User's avatar"> 
+                            <img class="user-list-item__gravatar avatar-sm rounded-circle" alt="" src="data:image/webp;base64,<?php echo base64_encode($user['user_imgdata']); ?>" width="48" alt="User's avatar"> 
                         </div>
                         
                         <div class="text-center text-lg-left ml-lg-2">
-                            <h5 class="card-title mb-0"><?= $user['user_name']; ?></h5>
-                            <div class="small text-secondary mt-0"><?= $user['user_sign']; ?></div>
+                            <h5 class="user-list-item__name card-title mb-0"><?= $user['user_name']; ?></h5>
+                            <div class="user-list-item__sign small text-secondary mt-0"><?= $user['user_sign']; ?></div>
                         </div>
                         
                     </div>
